c82cd0080696b55a771c497ff9bbae9205a20bf6
sam-liem <8178799+sam-liem@users.noreply.github.com>
2018-02-17 16:43:54 +0000

    made successful call to api

--- app.py
+++ app.py
@@ -70,6 +70,4 @@ if __name__ == '__main__':
 
     print("Starting app on port %d" % port)
 
-app = StarlingAccount()
-
-app.app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=False, port=port, host='0.0.0.0')
--- starling.py
+++ starling.py
@@ -1,18 +1,20 @@
-class StarlingAccount(object):
-    # Flask app should start in global layout
-    app = Flask(__name__)
-    @app.route('/webhook', methods=['POST'])
-    #Step one, user needs to login
+import requests
 
-    def __init__ (self):
+class account:
+    def __init__ (self,token):
         self.loggin = False
-
-    def testRedirection(self):
-        url = "https://oauth.starlingbank.com/?client_id=TJ71XnestPKk1kYi4aEM&response_type=json&state=$state&redirect_uri=$https://google.com"
+        self.token = token
         
-    def testRedirection2(self):
-        #What i did
-        url1 = "https://oauth.starlingbank.com/?client_id=TJ71XnestPKk1kYi4aEM&response_type=code&state=$state&redirect_uri=$https://google.com"
-        url2 = "https://oauth.starlingbank.com/?client_id=ecb8ffbf-2c5b-4e2b-8635-0a75d3b6e485&response_type=code&state=$state&redirect_uri=$https://google.com"
-        foo = urlopen(url)
-        print(urlopen(url).read())
+    # def testRedirection2(self):
+    #     url1 = "https://oauth.starlingbank.com/?client_id="+self.client_secret+"&response_type=code&state=$state&redirect_uri=$https://google.com"
+    #     url2 = "https://oauth.starlingbank.com/?client_id=ecb8ffbf-2c5b-4e2b-8635-0a75d3b6e485&response_type=code&state=$state&redirect_uri=$https://google.com"
+    #     foo = urlopen(url)
+    #     print(urlopen(url).read())
+
+    def getBalance(self):
+        headers = {"Accept": "application/json","Content-Type": "application/json","Authorization": "Bearer "+self.token}
+        req = requests.get('https://api-sandbox.starlingbank.com/api/v1/accounts/balance', headers=headers)
+        print(req)
+
+acc = account("1rxRXmg4lNh5rphevZwWNG1CYbTwRC9juFJe3ZGEenYo1wuStaXh2UZgMpNs9Pta")
+acc.getBalance()
