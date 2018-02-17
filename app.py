e97e6257e21608ad1fb3a0550c9fe3b2ec31dc8a
Kenny Macheka <knm512@yahoo.co.uk>
2018-02-17 15:58:13 +0000

    some stuff

--- app.py
+++ app.py
@@ -14,9 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
 from __future__ import print_function
 from future.standard_library import install_aliases
 install_aliases()
+"""
 
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
@@ -25,92 +27,114 @@ from urllib.error import HTTPError
 import json
 import os
 
+
 from flask import Flask
 from flask import request
 from flask import make_response
 
-# Flask app should start in global layout
-app = Flask(__name__)
+class StarlingAccount(object):
+    # Flask app should start in global layout
+    app = Flask(__name__)
+    @app.route('/webhook', methods=['POST'])
+    #Step one, user needs to login
+
+    def __init__ (self):
+        self.loggin = False
+
+    def testRedirection(self):
+        url = "https://oauth.starlingbank.com/?client_id=TJ71XnestPKk1kYi4aEM&response_type=json&state=$state&redirect_uri=$https://google.com"
+        
+    def testRedirection2(self):
+        #What i did
+        url1 = "https://oauth.starlingbank.com/?client_id=TJ71XnestPKk1kYi4aEM&response_type=code&state=$state&redirect_uri=$https://google.com"
+        url2 = "https://oauth.starlingbank.com/?client_id=ecb8ffbf-2c5b-4e2b-8635-0a75d3b6e485&response_type=code&state=$state&redirect_uri=$https://google.com"
+        foo = urlopen(url)
+        print(urlopen(url).read())
+
+    
+
+    def webhook():
+        #Gets json
+        req = request.get_json(silent=True, force=True)
+
+        print("Request:")
+        #prints out json as dictionary
+        print(json.dumps(req, indent=4))
+
+        res = processRequest(req)
+
+        #Converts to dictionary
+        res = json.dumps(res, indent=4)
+        # print(res)
+        r = make_response(res)
+        r.headers['Content-Type'] = 'application/json'
+        return r
 
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
-
-    print("Request:")
-    print(json.dumps(req, indent=4))
-
-    res = processRequest(req)
-
-    res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-
-def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
-        return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
+    def processRequest(req):
+        if req.get("result").get("action") != "yahooWeatherForecast":
+            return {}
+        
+        baseurl = "https://query.yahooapis.com/v1/public/yql?"
+        yql_query = makeYqlQuery(req)
+        if yql_query is None:
+            return {}
+        yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+        result = urlopen(yql_url).read()
+        print (result)
+        data = json.loads(result)
+        res = makeWebhookResult(data)
+        return res
 
 
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
+    def makeYqlQuery(req):
+        result = req.get("result")
+        parameters = result.get("parameters")
+        city = parameters.get("geo-city")
+        if city is None:
+            return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+        return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
-def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
+    def makeWebhookResult(data):
+        query = data.get('query')
+        if query is None:
+            return {}
 
-    result = query.get('results')
-    if result is None:
-        return {}
+        result = query.get('results')
+        if result is None:
+            return {}
 
-    channel = result.get('channel')
-    if channel is None:
-        return {}
+        channel = result.get('channel')
+        if channel is None:
+            return {}
 
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
+        item = channel.get('item')
+        location = channel.get('location')
+        units = channel.get('units')
+        if (location is None) or (item is None) or (units is None):
+            return {}
 
-    condition = item.get('condition')
-    if condition is None:
-        return {}
+        condition = item.get('condition')
+        if condition is None:
+            return {}
 
-    # print(json.dumps(item, indent=4))
+        # print(json.dumps(item, indent=4))
 
-    speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
-             ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
+        speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
+                 ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
-    print("Response:")
-    print(speech)
+        print("Response:")
+        print(speech)
 
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
+        return {
+            "speech": speech,
+            "displayText": speech,
+            # "data": data,
+            # "contextOut": [],
+            "source": "apiai-weather-webhook-sample"
+        }
 
 
 if __name__ == '__main__':
@@ -118,4 +142,6 @@ if __name__ == '__main__':
 
     print("Starting app on port %d" % port)
 
-    app.run(debug=False, port=port, host='0.0.0.0')
+app = StarlingAccount()
+
+app.app.run(debug=False, port=port, host='0.0.0.0')
