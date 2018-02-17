07ee17f3f5805a8861c1aba0929551736506f8e5
max <maxtaylordavies@gmail.com>
2018-02-17 22:39:08 +0000

    uploaded frontend files

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { ChatComponent } from './components/chat/chat.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  entryComponents: [ ChatComponent ]
+})
+export class AppModule { }
---
+++ frontend/src/app/dialog-flow-client/dialog-flow.client.ts
@@ -0,0 +1,3 @@
+import { ApiAiClient } from "api-ai-javascript";
+
+export const client = new ApiAiClient({accessToken: '9e75ac7ab0a74889a00db465826fcf42'});
