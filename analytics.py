ea647b30b7fd9ace7545353b66da1fe1d215ddb5
sam-liem <8178799+sam-liem@users.noreply.github.com>
2018-02-18 00:15:10 +0000

    added highest cost analytics

--- analytics.py
+++ analytics.py
@@ -1,30 +1,51 @@
-class Analytics(object):
-    def __init__ (self, transactions):
-        self.transactions = transactions
+import operator
 
+# class Analytics(object):
+#     def __init__ (self, transactions):
+#         self.transactions = transactions
     
-     def analyse(self):
-         self.alterDates()
-         self.processTransactions()
+#     def analyse(self):
+#         self.alterDates()
+#         self.processTransactions()
 
-     #Get a datetime object based on the first data and time noticed
-     def getBaseDate (self):
-         date = self.transactions[0]['created']
-         date[date.index("T")] = '-'
-         #format: yyyy-mm-ddThh-mm-ss
-         tokens = date.split("-")
-         datetimeObj = datetime(tokens[0],
-                                tokens[1],
-                                tokens[2],
-                                tokens[3]
-                                tokens[4],
-                                tokens[5])
+#     # Get a datetime object based on the first data and time noticed
+#     def getBaseDate (self):
+#         date = self.transactions[0]['created']
+#         date[date.index("T")] = '-'
+#         #format: yyyy-mm-ddThh-mm-ss
+#         tokens = date.split("-")
+#         datetimeObj = datetime(tokens[0],
+#                                 tokens[1],
+#                                 tokens[2],
+#                                 tokens[3]
+#                                 tokens[4],
+#                                 tokens[5])
                                 
-     def alterDates (self):
-         dateObj = getBaseDate()
+#     def alterDates (self):
+#         dateObj = getBaseDate()
          
-         #To alter a data, I'll take the start data and time
-         #Then I need to increase the time and dat
+#         #To alter a data, I'll take the start data and time
+#         #Then I need to increase the time and dat
         
-     def processTransactions(self):
-         self.alterDates()
+#     def processTransactions(self):
+#         self.alterDates()
+
+
+def highestExpense(transactions):
+    transactions = transactions['_embedded']['transactions']
+
+    # get most recent month's transactions?
+
+    narrativeCostTable = {}
+    for transaction in transactions:
+        if (transaction['direction'] == "OUTBOUND") and (transaction['narrative'] != "External Payment") and (transaction['narrative'] != "Mastercard"):
+            if (transaction['narrative'] in narrativeCostTable):
+                narrativeCostTable[transaction['narrative']] += int(transaction['amount'])
+            else:
+                narrativeCostTable[transaction['narrative']] = int(transaction['amount'])
+
+    narrativeCostTable = sorted(narrativeCostTable.items(), key=operator.itemgetter(1))
+
+    name, spent = narrativeCostTable[0]
+    speech = "You spent " + str(spent * -1) + " on " + name + ". Consider switching to cheaper alternatives."
+    print(speech)
